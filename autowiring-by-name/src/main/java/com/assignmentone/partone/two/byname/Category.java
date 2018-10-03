27839ea4061ccd0c0b2b38ecb8d0a5468b48a2ec
Bianca Annoscia <bianca.annoscia@gmail.com>
2018-10-03 14:23:07 -0400

    comments

--- autowiring-by-name/src/main/java/com/assignmentone/partone/two/byname/Category.java
+++ autowiring-by-name/src/main/java/com/assignmentone/partone/two/byname/Category.java
@@ -11,14 +11,21 @@ public class Category {
 	
 	private Long id = 1L; 
 	private String name = "Poetry"; 
+	
+	//when targetByName bean is grabbed from the xml properties file, this class will get analyzed
+	//for any property names that match the names of any bean ids in application context. 
+	//since this class has a property called bookFrost, and application context contains
+	//a bean id = bookFrost, that bean will get injected here using the setter method below
 	private static Book bookFrost;
 	
+	//since the method name has bookFrost in the name, this method will be used to inject the bookFrost bean
 	public void setBookFrost(Book bookFrost) {
         this.bookFrost = bookFrost;
     }
 	
 	
 	public static void main(String... args) {
+		//xml configuration is used here
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
         ctx.load("classpath:app-context.xml");
         ctx.refresh();
