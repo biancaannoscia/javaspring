38882a8ff27faa4a7fc22b2df0eb61f6abed5e81
Bianca Annoscia <bianca.annoscia@gmail.com>
2018-10-03 10:41:19 -0400

    term project

--- term-project/src/main/java/com/partone/assignmentone/ApplicationDriver.java
+++ term-project/src/main/java/com/partone/assignmentone/ApplicationDriver.java
@@ -7,31 +7,30 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 public class ApplicationDriver {
 
 	public static void main(String[] args) {
+		
+		//XML configuration was used for this application
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
 		ctx.load("classpath:app-context-xml.xml");
 		ctx.refresh();
-
+		
+		//a category bean with bookSet populated is created
 		Category categorySet = ctx.getBean("categorySet", Category.class);
-
 		categorySet.setId(2L);
 		categorySet.setName("Novels");
-
 		categorySet.displayInfo();
 
+		//a category bean with bookList populated is created
 		Category categoryList = ctx.getBean("categoryList", Category.class);
-
 		categoryList.setId(3L);
 		categoryList.setName("Java");
 		List<Book> bookList= categoryList.getBooksList();
 		bookList.add(new Book());
-
 		categoryList.displayInfo();
 
+		//a category bean with bookMap populated is created
 		Category categoryMap = ctx.getBean("categoryMap", Category.class);
-
 		categoryMap.setId(4L);
 		categoryMap.setName("Poetry");
-
 		categoryMap.displayInfo();
 
 	}
